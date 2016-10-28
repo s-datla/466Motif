8f68e752d5d48fcc698141af7ab27226c6acd871
caschenb <celine.aschen@gmail.com>
2016-10-28 12:26:15 -0500

    Steps 6->9 done. Benchmark created with 10 data set for each parameters combination

--- src/bioinfo/motif/Main.java
+++ src/bioinfo/motif/Main.java
@@ -1,134 +1,56 @@
 package bioinfo.motif;
-import java.util.*;
-import java.util.Random;
-import java.io.*;
+
+import java.io.File;
 
 public class Main {
-    public static double[][] randomMotif(int ML, double ICPC){
-		double[][] motif = new double[4][ML];
-		double[] columnValues = new double[ML];
-		double p;
-		if (ICPC == 1){
-			p=0.8105;
-		}
-		else if (ICPC == 1.5){
-			p=0.9245;
-		}
-		else if (ICPC == 2){
-			p=1;
-		}
-		else{
-			return null;
-		}
-		for (int i = 0; i<ML; i++){
-			int nucleotideToEmphasize = (int) (Math.random()*4);
-			//System.out.println(nucleotideToEmphasize);
-			for (int j = 0; j<4; j++){
-				motif[j][i] = (1-p);
-			}
-			motif[nucleotideToEmphasize][i]=p;
-		}
-		return motif;
-	}
-	
-    private int icpc = 0;
-    private int motifLength = 0;
-    private int sequenceLength = 20;
-    private int sequenceCount = 0;
+    private int defaultIcpc = 2;
+    private int defaultMl = 8;
+    private int defaultSl = 500;
+    private int defaultSC = 10;
 
     public static void main(String[] args) {
         new Main().run();
     }
 
-    void run(){
-        String motif = "AGT";
-        takeInput();
-        String s = generateSequences();
-        String news = plantMotif(motif,s);
-        printFASTA(news, 1);
-    }
+    void run() {
+        String directory = "benchmark";
+        Benchmark benchmark = new Benchmark();
 
-    void takeInput(){
+        double[] icpc = {1, 1.5};
+        int[] ml = {6, 7};
+        int[] sc = {5, 20};
 
-        Scanner input = new Scanner(System.in);
-        while(true){
-            System.out.println("Please enter the Information Content Per Column (ICPC): ");
-            try {
-                this.icpc = input.nextInt();
-            } catch (Exception e){
-                System.out.println("Invalid Entry, please try again");
-                input.nextLine();
-                continue;
+        String dataSet = directory + "/defaultParameter";
+        benchmark.createDataSet(defaultIcpc, defaultMl, defaultSl, defaultSC, dataSet);
+
+        for (int i = 0; i < icpc.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                dataSet = directory + "/icpc=" + icpc[i] + "&num=" + j;
+                File dir = new File(dataSet);
+                dir.mkdirs();
+                benchmark.createDataSet(icpc[i], defaultMl, defaultSl, defaultSC, dataSet);
             }
-            System.out.println("Please enter the Motif Length: ");
-            try {
-                this.motifLength= input.nextInt();
-            } catch (Exception e){
-                System.out.println("Invalid Entry, please try again");
-                input.nextLine();
-                continue;
+        }
+
+        for (int i = 0; i < ml.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                dataSet = directory + "/ml=" + ml[i] + "&num=" + j;
+                File dir = new File(dataSet);
+                dir.mkdirs();
+                benchmark.createDataSet(defaultIcpc, ml[i], defaultSl, defaultSC, dataSet);
             }
-            System.out.println("Please enter the Sequence Length: ");
-            try {
-                this.sequenceLength= input.nextInt();
-            } catch (Exception e){
-                System.out.println("Invalid Entry, please try again");
-                input.nextLine();
-                continue;
+        }
+
+        for (int i = 0; i < sc.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                dataSet = directory + "/sc=" + sc[i] + "&num=" + j;
+                File dir = new File(dataSet);
+                dir.mkdirs();
+                benchmark.createDataSet(defaultIcpc, defaultMl, defaultSl, sc[i], dataSet);
             }
-            System.out.println("Please enter the Sequence Count: ");
-            try {
-                this.sequenceCount= input.nextInt();
-            } catch (Exception e){
-                System.out.println("Invalid Entry, please try again");
-                input.nextLine();
-                continue;
-            }
-            break;
         }
 
 
     }
 
-    String generateSequences(){
-        Random newr = new Random();
-        String sequence = "";
-        String nucleotides = "ACGT";
-        for(int i =0; i < this.sequenceLength; i++){
-            int nextval = newr.nextInt(4);
-            sequence += nucleotides.charAt(nextval);
-        }
-        return sequence;
-    }
-
-    String plantMotif(String motif, String sequence){
-        Random newr = new Random();
-        int newPosition = newr.nextInt(sequence.length() - motif.length());
-        System.out.println(newPosition);
-        char[] chars = sequence.toCharArray();
-        char[] motifc = motif.toCharArray();
-        for(int i =0; i < motif.length(); i++){
-            chars[newPosition+i] = motifc[i];
-        }
-        String finalseq = String.valueOf(chars);
-        System.out.println(sequence + "\n" + finalseq + "\n");
-        return finalseq;
-    }
-
-    void printFASTA(String s, int num){
-        try {
-            File f = new File("seq1.fa");
-            if (!(f.exists() && !f.isDirectory())) {
-                    f.createNewFile();
-            }
-            FileWriter fw = new FileWriter(f);
-            System.out.println("REACHED CHECKPOINT 1");
-            fw.write(">SEQUENCE " + num + "\n" + s);
-            fw.flush();
-            fw.close();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
 }
