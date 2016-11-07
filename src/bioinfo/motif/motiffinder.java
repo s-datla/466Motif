f4f772ff617679a6ae4603dc11acc35de5073337
caschenb <celine.aschen@gmail.com>
2016-11-07 15:02:15 -0600

    calculation of background probabilities + PWM + general algorithm

--- src/bioinfo/motif/motiffinder.java
+++ src/bioinfo/motif/motiffinder.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MotifFinder {
     private List<String> sequences;
@@ -60,8 +61,8 @@ public class MotifFinder {
         String sequence = null;
         List<String> sequences = new ArrayList<String>();
         try {
-            inputStream.readLine();
             inputStream = new BufferedReader(new FileReader(directory + "/sequences.fa"));
+            inputStream.readLine();
             while ((sequence = inputStream.readLine()) != null) {
                 sequences.add(sequence);
             }
@@ -95,9 +96,97 @@ public class MotifFinder {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("hi");
+
+        Random randomizer = new Random();
+        int r = 0;
+        String seq;
+        int pos;
+        List<String> otherSequences = new ArrayList<String>();
+        double[][] PWM;
+        double[] probs;
+        List<Integer> positions = new ArrayList<Integer>();
+        int lengthSeq = sequences.get(0).length();
+        //we initialize randomly each position
+        for (int i = 0; i<sequences.size() ; i++) {
+            positions.add(randomizer.nextInt(lengthSeq - ML));
+        }
+
+        //for now the condition is a number of iterations, but we can change it for a duration when our algorithm works
+        int K = 1000;
+        for (int k=0; k<K; k++) {
+            otherSequences = sequences;
+            r = randomizer.nextInt(otherSequences.size());
+            seq = otherSequences.remove(r);
+            //I'm not sure that we need the previous position of the sequence z
+            pos = positions.remove(r);
+            PWM = calculatePWM(otherSequences, positions);
+            probs = calculateP(otherSequences);
+
+            /* HERE WE CAN CALCULATE THE SCORE AND UPDATE THE POSITION FOR THE PARTICULAR SEQUENCE*/
+
+
+        }
+    }
+
+    //calculate the probability of generating x according to the background model (=~1/4 for each nucleotide)
+    private double[] calculateP (List<String> sequences) {
+        double[] probabilities = new double[4];
+        int N = 0;
+        for (String seq : sequences) {
+            for (int i = 0; i<seq.length(); i++) {
+                if (seq.charAt(i)=='A'){
+                    probabilities[0] ++;
+                }
+                else if (seq.charAt(i)=='C'){
+                    probabilities[1] ++;
+                }
+                else if (seq.charAt(i)=='G'){
+                    probabilities[2] ++;
+                }
+                else if (seq.charAt(i)=='T'){
+                    probabilities[3] ++;
+                }
+                N++;
+            }
+        }
+        for (int i = 0; i<4; i++) {
+            probabilities[i] /= N;
+        }
+        return probabilities;
+    }
+
+    //calculate the position weight matrix
+    private double[][] calculatePWM(List<String> sequences, List<Integer> positions) {
+        double[][] PWM = new double[ML][4];
+        for (int i = 0; i<sequences.size(); i++) {
+            String seq = sequences.get(i);
+            for (int j = 0; j<ML; j++){
+                int pos = j + positions.get(i);
+                if (seq.charAt(pos)=='A'){
+                    PWM[j][0] ++;
+                }
+                else if (seq.charAt(pos)=='C'){
+                    PWM[j][1] ++;
+                }
+                else if (seq.charAt(pos)=='G'){
+                    PWM[j][2] ++;
+                }
+                else if (seq.charAt(pos)=='T'){
+                    PWM[j][3] ++;
+                }
+            }
+        }
+        //pseudo-count of 0.25 + transformation in probability 0<PWM<1
+        for (int j = 0; j<ML; j++){
+            for (int k = 0; k<4; k++) {
+                PWM[j][k] = (PWM[j][k] + 0.25) / 10 ;
+            }
+        }
+        return PWM;
     }
 
     public MotifFinder() {
     }
+
+
 }
